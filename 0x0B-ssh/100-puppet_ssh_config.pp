6df51a14b27628e9e08b5c0a12ad9d30c96ab112
AmrTaman <amrtaman4325@gmail.com>
2024-04-22 03:11:29 +0000

    sd

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,25 @@
-#iam here
-include stdlib
+# Define SSH client configuration file path
+$ssh_config_file = '/home/vagrant/.ssh/config'
 
-file_line {'configuring password auth':
-ensure => 'present',
-path   => '/etc/ssh/ssh_config',
-match  => '#   PasswordAuthentication yes',
-line   => '#   PasswordAuthentication no',
-}
+# Ensure SSH client configuration file exists
+ file { $ssh_config_file:
+   ensure => present,
+     owner  => 'vagrant',
+       group  => 'vagrant',
+         mode   => '0600',
+         }
+
+         # Ensure SSH client configuration includes private key and disables password authentication
+         file_line { 'Declare identity file':
+           path    => $ssh_config_file,
+             line    => '    IdentityFile ~/.ssh/school',
+               match   => '^#?IdentityFile',
+                 ensure  => present,
+                 }
+
+                 file_line { 'Turn off passwd auth':
+                   path    => $ssh_config_file,
+                     line    => '    PasswordAuthentication no',
+                       match   => '^#?PasswordAuthentication',
+                        ensure  => present,
+                         }
