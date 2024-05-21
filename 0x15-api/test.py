e9b2c58e70fffe5bbdca7570f6bf0dadc26b8d10
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2024-05-21 04:57:49 +0300

    Update test.py

--- 0x15-api/test.py
+++ 0x15-api/test.py
@@ -12,9 +12,24 @@ todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 def first_line(id):
     """ Fetch user name """
+
     resp = requests.get(users_url).json()
+
     name = None
     for i in resp:
         if i['id'] == id:
             name = i['name']
-    print(name)
+
+    filename = 'amr'
+
+    with open(filename, 'r') as f:
+        first = f.readline().strip()
+
+    if name in first:
+        print("Employee Name: OK")
+    else:
+        print("Employee Name: Incorrect")
+
+
+if __name__ == "__main__":
+    first_line(int(sys.argv[1]))
