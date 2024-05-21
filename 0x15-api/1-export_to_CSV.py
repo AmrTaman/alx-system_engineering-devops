8bd6789555b4bababc4edde2ce638c785c9eac5d
AmrTaman <amrtaman4325@gmail.com>
2024-05-21 02:33:34 +0000

    d

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,12 +11,12 @@ if __name__ == "__main__":
     employee_id = sys.argv[1]
     r_user = requests.get("https://jsonplaceholder.typicode.com"
                           "/users/{}".format(employee_id))
-    name = r_user.json().get('name')
+    name = r_user.json().get('username')
     r_todo = requests.get("https://jsonplaceholder.typicode.com"
                           "/todos?userId={}".format(employee_id))
     todo_data = r_todo.json()
     with open("{}.csv".format(employee_id), 'w') as f:
         csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for i in todo_data:
-            csv_writer.writerow([employee_id, name, task['completed'],
-                                 task['title']])
+            csv_writer.writerow([employee_id, name, i['completed'],
+                                 i['title']])
