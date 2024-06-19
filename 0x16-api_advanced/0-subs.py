94de978c2fcd2e627211557649481705763b6642
AmrTaman <amrtaman4325@gmail.com>
2024-06-19 19:14:13 +0000

    d

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,8 @@ def number_of_subscribers(subreddit):
     this function counts number of sub
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    res = requests.get(url, allow_redirects=False, headers=headers)
     if res.status_code == 200:
         data = res.json()['data']
         return (data['subscribers'])
