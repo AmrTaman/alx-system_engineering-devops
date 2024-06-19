1a6e02119e1e2f81b2854faad6912fd2a9b53d3c
AmrTaman <amrtaman4325@gmail.com>
2024-06-19 22:43:24 +0000

    d

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,10 +16,10 @@ def top_ten(subreddit):
     headers = {'User-agent': 'python-requests/2.22.0'}
     resp = requests.get(url, params=params,
                         headers=headers, allow_redirects=False)
+    if resp.status_code == 404:
+        print("None")
     if resp.status_code == 200:
         posts_lst = resp.json()['data']['children']
         for post in posts_lst:
             data = post['data']
             print(data['title'])
-    else:
-        print("None")
